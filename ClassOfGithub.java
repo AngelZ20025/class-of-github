114c0f7a1b6f34a8031a63549ad424aee4d8669d
AngelZ20025 <2642527@sd61learn.ca>
2024-11-07 17:20:47 -0800

    Create ClassOfGithub.java
    class of today

---
+++ ClassOfGithub.java
@@ -0,0 +1,13 @@
+public class ClassOfGithub {
+public static void main(String[] args) {
+    System.out.print("Hello World");
+    String striNgname ="Angel";
+    System.out.print(striNgname);
+    int intName = 2;
+    float floatName =2.99f;
+    double doubleName = 5.531;
+    char charName ='D';
+    boolean booleanName = true;
+}
+
+}
